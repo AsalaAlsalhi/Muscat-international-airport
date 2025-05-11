e1766ebd3e7e1794c9834405feb99098873473c7
ahmed <ahmed@AHMEDPC>
2025-05-11 16:13:18 +0400

    Add project files.

---
+++ Context/ApplicationDbcontext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Air1.Context
+{
+    public class ApplicationDbcontext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server = . ; initial Catalog = Asala ; Trust Connection = True ; Trust Server Certificate = True ");
+        }
+    }
+}
